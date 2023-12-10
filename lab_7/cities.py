962b9cfa7df4b699cf567fbe00b641211e686ad5
Andreev Alexander <andreev.business.mail@gmail.com>
2023-12-10 23:39:11 +0300

    Updated multiple choice and complicated logics

--- lab_7/cities.py
+++ lab_7/cities.py
@@ -6,190 +6,201 @@ from main import parser
 ods_path = "./Cities_v4.ods"
 
 with open("./cities.txt", encoding='utf-8') as file:
-	cities_title = [row.strip() for row in file]
+    cities_title = [row.strip() for row in file]
 
 cities = read_ods(ods_path, 1, headers=True)
 cities.index = cities.index + 1
+
 
 # print(cities)
 
 
 def full_list():
-	print('\nСписок доступных городов:')
-	for city in cities_title:
-		print(city)
+    print('\nСписок доступных городов:')
+    for city in cities_title:
+        print(city)
 
 
 def random_city():
-	print('\n', cities.iloc[random.randint(0, 30)], '\n')
+    print('\n', cities.iloc[random.randint(0, 30)], '\n')
 
 
 def find_city(title):
-	num_cities = cities.shape[0]
-	for i in range(num_cities):
-		
-		if cities.iloc[i]['Город'].lower() == title.lower():
-			print('\n', cities.iloc[i], '\n')
-			return
-	
-	return -1
+    num_cities = cities.shape[0]
+    for i in range(num_cities):
+
+        if cities.iloc[i]['Город'].lower() == title.lower():
+            print('\n', cities.iloc[i], '\n')
+            return
+
+    return -1
 
 
 def output_all_cities(res_cities):
-	for i in (res_cities):
-		print('\n', i, '\n')
-		print('---------------------\n')
+    for i in (res_cities):
+        print('\n', i, '\n')
+        print('---------------------\n')
 
 
 def output_cities(phrase, selected_attributes):
-	print(phrase, selected_attributes)
-	count = 0
-	num_cities = cities.shape[0]
-	if (selected_attributes == 'Цена'):
-		phrase_word = phrase[0]
-		for i in range(num_cities):
-			if phrase_word == "дешево":
-				if cities.iloc[i][selected_attributes] < 6000:
-					count = 1
-					print('\n', cities.iloc[i], '\n')
-					print('---------------------\n')
-			elif phrase_word == "нормально":
-				if cities.iloc[i][selected_attributes] >= 6000 and cities.iloc[i][selected_attributes] < 6800:
-					count = 1
-					print('\n', cities.iloc[i], '\n')
-					print('---------------------\n')
+    print(phrase, selected_attributes)
+    count = 0
+    num_cities = cities.shape[0]
+    if (selected_attributes == 'Цена'):
+        phrase_word = phrase[0]
+        for i in range(num_cities):
+            if phrase_word == "дешево":
+                if cities.iloc[i][selected_attributes] < 6000:
+                    count = 1
+                    print('\n', cities.iloc[i], '\n')
+                    print('---------------------\n')
+            elif phrase_word == "нормально":
+                if cities.iloc[i][selected_attributes] >= 6000 and cities.iloc[i][selected_attributes] < 6800:
+                    count = 1
+                    print('\n', cities.iloc[i], '\n')
+                    print('---------------------\n')
 
-			elif phrase_word == "доступно":
-				if cities.iloc[i][selected_attributes] >= 6800 and cities.iloc[i][selected_attributes] <= 7200:
-					count = 1
-					print('\n', cities.iloc[i], '\n')
-					print('---------------------\n')
+            elif phrase_word == "доступно":
+                if cities.iloc[i][selected_attributes] >= 6800 and cities.iloc[i][selected_attributes] <= 7200:
+                    count = 1
+                    print('\n', cities.iloc[i], '\n')
+                    print('---------------------\n')
 
-			elif phrase_word == "комфортно":
-				if cities.iloc[i][selected_attributes] >= 7200 and cities.iloc[i][selected_attributes] < 7800:
-					count = 1
-					print('\n', cities.iloc[i], '\n')
-					print('---------------------\n')
+            elif phrase_word == "комфортно":
+                if cities.iloc[i][selected_attributes] >= 7200 and cities.iloc[i][selected_attributes] < 7800:
+                    count = 1
+                    print('\n', cities.iloc[i], '\n')
+                    print('---------------------\n')
 
-			elif phrase_word == "дорого":
-				if cities.iloc[i][selected_attributes] >= 7800:
-					count = 1
-					print('\n', cities.iloc[i], '\n')
-					print('---------------------\n')
-	else:
-	
+            elif phrase_word == "дорого":
+                if cities.iloc[i][selected_attributes] >= 7800:
+                    count = 1
+                    print('\n', cities.iloc[i], '\n')
+                    print('---------------------\n')
+    else:
 
-		for i in range(num_cities):
-			if (cities.iloc[i][selected_attributes] and 
-				cities.iloc[i][selected_attributes].lower() in set(phrase)):
-				count = 1
-				print('\n', cities.iloc[i], '\n')
-				print('---------------------\n')
+        for i in range(num_cities):
+            if (cities.iloc[i][selected_attributes] and
+                    cities.iloc[i][selected_attributes].lower() in set(phrase)):
+                count = 1
+                print('\n', cities.iloc[i], '\n')
+                print('---------------------\n')
 
-	if count == 0:
-		print('\nТакого не существует...')
+    if count == 0:
+        print('\nТакого не существует...')
 
-def locations_city(phrase, tmp_cities, locations):
-	res_cities = []
 
-	intersection = set(phrase) & locations
+def text_values_city(phrase, tmp_cities, text_fields):
+    res_cities = []
 
-	for tmp_city in tmp_cities:
-		if len(set(parser(tmp_city["Локация"])) & intersection) != 0:
-			res_cities.append(tmp_city)
+    intersection = set(phrase) & text_fields
 
-	if len(res_cities) != 0:
-		print("if len(res_cities) != 0:")
-		return res_cities
-	return tmp_cities
+    print(intersection)
+
+    for i in tmp_cities:
+        tmp_text_values = i["Локация"] + " " + i["Тематика"] + " " + i["Направление"] + " " + i["Область"] + " "
+        if i["Вид природы"] is not None:
+            tmp_text_values += i["Вид природы"] + " "
+        if i["Историческая эпоха"] is not None:
+            tmp_text_values += i["Историческая эпоха"] + " "
+        if i["Историческая личность"] is not None:
+            tmp_text_values += i["Историческая личность"] + " "
+        if len(set(parser(tmp_text_values)) &
+               intersection) == len(intersection):
+            res_cities.append(i)
+
+    if len(res_cities) != 0:
+        print("if len(res_cities) != 0:")
+        return res_cities, 1
+    return tmp_cities, 0
+
 
 def price_city(phrase, tmp_cities):
+    res_cities = []
 
-	res_cities = []
+    selected_attributes = "Цена"
+    count = 0
+    num_cities = cities.shape[0]
 
-	selected_attributes = "Цена"
-	count = 0
-	num_cities = cities.shape[0]
-	
-	phrase_word = phrase[0]
-	for i in (tmp_cities):
-		if ('дёшевый' in set(phrase) or 'дёшево' in set(phrase)):
-			if i[selected_attributes] < 6000:
-				res_cities.append(i)
-		elif ('нормальный' in set(phrase) or 'нормально' in set(phrase)):
-			if i[selected_attributes] >= 6400 and i[selected_attributes] < 6800:
-				res_cities.append(i)
-		elif ('доступный' in set(phrase) or 'доступно' in set(phrase)):
-			if i[selected_attributes] >= 6800 and i[selected_attributes] <= 7200:
-				res_cities.append(i)
-		elif ('комфортный' in set(phrase) or 'комфортно' in set(phrase)):
-			if i[selected_attributes] >= 7200 and i[selected_attributes] < 7800:
-				res_cities.append(i)
-		elif ('дорого' in set(phrase) or 'дорогой' in set(phrase)):
-			if i[selected_attributes] >= 7800:
-				res_cities.append(i)
+    phrase_word = phrase[0]
+    for i in (tmp_cities):
+        if ('дёшевый' in set(phrase) or 'дёшево' in set(phrase)):
+            if i[selected_attributes] < 6000:
+                res_cities.append(i)
+        elif ('нормальный' in set(phrase) or 'нормально' in set(phrase)):
+            if i[selected_attributes] >= 6400 and i[selected_attributes] < 6800:
+                res_cities.append(i)
+        elif ('доступный' in set(phrase) or 'доступно' in set(phrase)):
+            if i[selected_attributes] >= 6800 and i[selected_attributes] <= 7200:
+                res_cities.append(i)
+        elif ('комфортный' in set(phrase) or 'комфортно' in set(phrase)):
+            if i[selected_attributes] >= 7200 and i[selected_attributes] < 7800:
+                res_cities.append(i)
+        elif ('дорого' in set(phrase) or 'дорогой' in set(phrase)):
+            if i[selected_attributes] >= 7800:
+                res_cities.append(i)
 
-	if len(res_cities) != 0:
-		print("if len(res_cities) != 0:")
-		return res_cities
-	return tmp_cities
+    if len(res_cities) != 0:
+        print("if len(res_cities) != 0:")
+        return res_cities, 1
+    return tmp_cities, 0
 
 
 def distance_city(phrase, tmp_cities):
-	res_cities = []
+    res_cities = []
 
-	if ('не' in set(phrase) and 'очень' in set(phrase) and 
-		('близко' in set(phrase) or 'близкий' in set(phrase))):
+    if ('не' in set(phrase) and 'очень' in set(phrase) and
+            ('близко' in set(phrase) or 'близкий' in set(phrase))):
 
-		for i in (tmp_cities):
-			if (i['Расстояние от Москвы'] >= 100 and
-				i['Расстояние от Москвы'] <= 300):
-				res_cities.append(i)
-
-
-
-	elif ('не' in set(phrase) and 'очень' in set(phrase) and 
-		('далеко' in set(phrase) or 'далёкий' in set(phrase))):
-		count = 0
-		num_cities = cities.shape[0]
-
-		for i in (tmp_cities):
-			if (i['Расстояние от Москвы'] <= 100):
-				res_cities.append(i)
+        for i in (tmp_cities):
+            if (i['Расстояние от Москвы'] >= 100 and
+                    i['Расстояние от Москвы'] <= 300):
+                res_cities.append(i)
 
 
 
-	elif (len(set(phrase) & {'близко', 'близкий'}) != 0):
-		count = 0
-		num_cities = cities.shape[0]
+    elif ('не' in set(phrase) and 'очень' in set(phrase) and
+          ('далеко' in set(phrase) or 'далёкий' in set(phrase))):
+        count = 0
+        num_cities = cities.shape[0]
 
-		for i in (tmp_cities):
-			if (i['Расстояние от Москвы'] <= 200):
-				res_cities.append(i)
-
-		if count == 0:
-			print('\nТакого не существует...')
-
-	elif (len(set(phrase) & {'средне', 'средней'}) != 0):
-		count = 0
-		num_cities = cities.shape[0]
-
-		for i in (tmp_cities):
-			if (i['Расстояние от Москвы'] >= 300 and
-				i['Расстояние от Москвы'] <= 500):
-				res_cities.append(i)
+        for i in (tmp_cities):
+            if (i['Расстояние от Москвы'] <= 100):
+                res_cities.append(i)
 
 
 
-	elif (len(set(phrase) & {'далеко', 'далёкий'}) != 0):
-		count = 0
-		num_cities = cities.shape[0]
+    elif (len(set(phrase) & {'близко', 'близкий'}) != 0):
+        count = 0
+        num_cities = cities.shape[0]
 
-		for i in (tmp_cities):
-			if (i['Расстояние от Москвы'] >= 400):
-				res_cities.append(i)
+        for i in (tmp_cities):
+            if (i['Расстояние от Москвы'] <= 200):
+                res_cities.append(i)
 
-	if len(res_cities) != 0:
-		print("if len(res_cities) != 0:")
-		return res_cities
-	return cities.iloc
+        if count == 0:
+            print('\nТакого не существует...')
+
+    elif (len(set(phrase) & {'средне', 'средней'}) != 0):
+        count = 0
+        num_cities = cities.shape[0]
+
+        for i in (tmp_cities):
+            if (i['Расстояние от Москвы'] >= 300 and
+                    i['Расстояние от Москвы'] <= 500):
+                res_cities.append(i)
+
+
+
+    elif (len(set(phrase) & {'далеко', 'далёкий'}) != 0):
+        count = 0
+        num_cities = cities.shape[0]
+
+        for i in (tmp_cities):
+            if (i['Расстояние от Москвы'] >= 400):
+                res_cities.append(i)
+
+    if len(res_cities) != 0:
+        print("if len(res_cities) != 0:")
+        return res_cities, 1
+    return res_cities, 0
--- lab_7/main.py
+++ lab_7/main.py
@@ -6,165 +6,179 @@ from cities import *
 
 morph = pymorphy2.MorphAnalyzer()
 
-attributes = [["Город", "Город", "\nНапиши название."], 
-				["Тематика", "Тематика", "\nУкажи тематику."], 
-				["Вид природы", "Вид природы", "\nУкажи вид промысла."], 
-				["Историческая эпоха", "Историческая эпоха", "\nУкажи историческую эпоху."], 
-				["Историческая личность", "Историческая личность", "\nУкажи историческую личность."],
-				["Город-курорт", "Город-курорт", "\nГород-курорт?"], 
-				["Население", "Население", "\nУкажи население города."], 
-				["Цена", "Цена", "\nУкажи цену за 1 ночь (Дешево, Нормально, Доступно, Комфортно, Дорого)."], 
-				["Направление", "Направление", "\nУкажи направление."] , 
-				["Область", "Область", "\nУкажи область."], 
-				["Локация", "Локация", "\nУкажи локацию."]]
+attributes = [["Город", "Город", "\nНапиши название."],
+              ["Тематика", "Тематика", "\nУкажи тематику."],
+              ["Вид природы", "Вид природы", "\nУкажи вид промысла."],
+              ["Историческая эпоха", "Историческая эпоха", "\nУкажи историческую эпоху."],
+              ["Историческая личность", "Историческая личность", "\nУкажи историческую личность."],
+              ["Город-курорт", "Город-курорт", "\nГород-курорт?"],
+              ["Население", "Население", "\nУкажи население города."],
+              ["Цена", "Цена", "\nУкажи цену за 1 ночь (Дешево, Нормально, Доступно, Комфортно, Дорого)."],
+              ["Направление", "Направление", "\nУкажи направление."],
+              ["Область", "Область", "\nУкажи область."],
+              ["Локация", "Локация", "\nУкажи локацию."]]
 selected_attributes = ''
 
 step = 0
 scenarios = -1
 
+
+def get_text_values():
+    res_cities = cities.iloc
+
+    text_values = ""
+
+    for i in res_cities:
+        text_values += i["Локация"] + " " + i["Тематика"] + " " + i["Направление"] + " " + i["Область"] + " "
+        if i["Вид природы"] is not None:
+            text_values += i["Вид природы"] + " "
+        if i["Историческая эпоха"] is not None:
+            text_values += i["Историческая эпоха"] + " "
+        if i["Историческая личность"] is not None:
+            text_values += i["Историческая личность"] + " "
+
+
+    return set(parser(text_values[:-1]))
+
+
 def handle(phrase):
-	global step, scenarios, attributes, selected_attributes
+    global step, scenarios, attributes, selected_attributes
 
-	# Вся выборка
-	res_cities = cities.iloc
+    if len(set(phrase) & {'давать', 'привет', 'добрый', 'здравствуй', 'здравствуйте'}) != 0:
+        welcome()
+        return
 
-	locations = ""
+    if len(set(phrase) & {'пока', 'нет'}) != 0:
+        bye()
+        exit(0)
 
-	for i in res_cities:
-		locations += i["Локация"] + " "
+    if (len(set(phrase) & {'какой', 'вывести', 'показать', 'написать', 'перечислить', 'список', 'подсказать'}) != 0
+            and len(set(phrase) & {'всё', 'все', 'перечень', 'каталог', 'список'}) != 0
+            and len(set(phrase) & {'город'}) != 0):
+        full_list()
+        return
 
-	locations = locations[:-1]
+    if (len(set(phrase) & {'случайный', 'любой', 'какойнибудь'}) != 0):
+        random_city()
+        return
 
-	locations = set(parser(locations))
-	
-	if len(set(phrase) & {'давать', 'привет', 'добрый', 'здравствуй', 'здравствуйте'}) != 0:
-		welcome()
-		return
+    count_terms_worked = 0
+    res_cities = cities.iloc
 
-	if len(set(phrase) & {'пока', 'нет'}) != 0:
-		bye()
-		exit(0)
+    res_status = 1
 
-	if (len(set(phrase) & {'какой', 'вывести', 'показать', 'написать', 'перечислить', 'список', 'подсказать'}) != 0
-		and len(set(phrase) & {'всё' , 'все', 'перечень', 'каталог', 'список'}) != 0
-		and len(set(phrase) & {'город'}) != 0):
-		
-		full_list()
-		return
+    if len(set(phrase) & {'близко', 'близкий', 'далеко', 'далёкий', 'средне', 'средний', 'очень', 'не'}) != 0:
+        res_cities, status = distance_city(phrase, res_cities)
+        res_status *= status
+        count_terms_worked += 1
 
-	if (len(set(phrase) & {'случайный', 'любой', 'какойнибудь'}) != 0):
-		random_city()
-		return
+    if len(set(phrase) & {'дорогой', 'дорого', 'дёшево', 'дешевый', 'нормальный', 'нормально'}) != 0:
+        res_cities, status = price_city(phrase, res_cities)
+        res_status *= status
+        count_terms_worked += 1
 
-	count_terms_worked = 0
+    if len(set(phrase) & get_text_values()) != 0:
+        res_cities, status = text_values_city(phrase, res_cities, get_text_values())
+        res_status *= status
+        count_terms_worked += 1
 
-	if len(set(phrase) & {'близко', 'близкий', 'далеко', 'далёкий', 'средне', 'средний', 'очень', 'не'}) != 0:
-		res_cities = distance_city(phrase, res_cities)
-		count_terms_worked += 1
+    if count_terms_worked > 0:
+        if res_status == 1:
+            output_all_cities(res_cities)
+        else:
+            print("ТАКИХ НЕ НАЙДЕНО!!!!!")
+        more()
 
-	if len(set(phrase) & {'дорогой', 'дорого', 'дёшево', 'дешевый', 'нормальный', 'нормально'}) != 0:
-		res_cities = price_city(phrase, res_cities)
-		count_terms_worked += 1
+    if len(set(phrase) & {'какой', 'вывести', 'показать', 'найти', 'подсказать'}) != 0 or (
+            len(set(phrase) & {'какой', 'вывести', 'показать', 'найти', 'подсказать'}) != 0
+            and len(set(phrase) & {'город'}) != 0) or scenarios >= 3:
+        if scenarios == -1:
+            scenarios = 3
 
-	print(locations)
-	print(set(phrase) & locations)
-	if len(set(phrase) & locations) != 0:
-		res_cities = locations_city(phrase, res_cities, locations)
-		count_terms_worked += 1
+        if step == 0:
+            print('\nПоказать город по названию?')
+            string = input()
+            string_parser = parser(string)
 
-	if count_terms_worked > 0:
-		output_all_cities(res_cities)
-		more()
-		
+            if ('да') in string_parser:
+                step = 2
+                scenarios = 4
 
-	if len(set(phrase) & {'какой', 'вывести', 'показать', 'найти', 'подсказать'}) != 0 or (len(set(phrase) & {'какой', 'вывести', 'показать', 'найти', 'подсказать'}) != 0
-				and len(set(phrase) & {'город'}) != 0) or scenarios >= 3:
-		if scenarios == -1:
-			scenarios = 3
+                print('\nЧто за город?')
+                city_title = input()
 
-		if step == 0:
-			print('\nПоказать город по названию?')
-			string = input()
-			string_parser = parser(string)
+                step = 0
+                scenarios = -1
 
-			if ('да') in string_parser:
-				step = 2
-				scenarios = 4
-				
-				print('\nЧто за город?')
-				city_title = input()
-				
-				step = 0
-				scenarios = -1
+                city = find_city(city_title)
+                if city == -1:
+                    not_found()
+                else:
+                    more()
 
-				city = find_city(city_title)
-				if city == -1:
-					not_found()
-				else:
-					more()
+            elif 'нет' in string_parser:
+                step = 2
+                scenarios = 5
 
-			elif 'нет' in string_parser:
-				step = 2
-				scenarios = 5
-				
-				print('\nПо какому критерию будем искать?')
-			else:
-				step = 0
-				scenarios = -1
+                print('\nПо какому критерию будем искать?')
+            else:
+                step = 0
+                scenarios = -1
 
-				print(string)
-				city = find_city(city_title)
-				if city == -1:
-					not_found()
-				else:
-					more()
-			return
+                print(string)
+                city = find_city(city_title)
+                if city == -1:
+                    not_found()
+                else:
+                    more()
+            return
 
-		elif step == 2:
-			if scenarios == 5:
-				attr = ''
-				for i in range(len(attributes)):
-					
-					if attributes[i][1].lower() in set(phrase):
-						attr = attributes[i][0]
-						selected_attributes = attributes[i][0]
-						print(attributes[i][2])
-						step = 3
-						scenarios = 5
-				
-				if attr == '':
-					print('\nУверен, что не опечатался?')
-		
-		elif step == 3:
-			step = 0
-			scenarios = -1
+        elif step == 2:
+            if scenarios == 5:
+                attr = ''
+                for i in range(len(attributes)):
 
-			output_cities(phrase, selected_attributes)
-			more() 
+                    if attributes[i][1].lower() in set(phrase):
+                        attr = attributes[i][0]
+                        selected_attributes = attributes[i][0]
+                        print(attributes[i][2])
+                        step = 3
+                        scenarios = 5
 
-	else:
-		not_found()
-		more()
-	
-	return
+                if attr == '':
+                    print('\nУверен, что не опечатался?')
+
+        elif step == 3:
+            step = 0
+            scenarios = -1
+
+            output_cities(phrase, selected_attributes)
+            more()
+
+    else:
+        not_found()
+        more()
+
+    return
 
 
 def parser(s):
-	phrase = re.sub(r'[^\w\s]', '', s.lower()).split()
-	norm_phrase = list()
-	for word in phrase:
-		norm_phrase.append(morph.parse(word)[0].normal_form)
-	return norm_phrase
+    phrase = re.sub(r'[^\w\s]', '', s.lower()).split()
+    norm_phrase = list()
+    for word in phrase:
+        norm_phrase.append(morph.parse(word)[0].normal_form)
+    return norm_phrase
 
-  
+
 def main():
-	welcome()
+    welcome()
 
-	while True:
-		phrase = input()
-		norm_phrase = parser(phrase)
-		#print(norm_phrase)
-		handle(norm_phrase)
+    while True:
+        phrase = input()
+        norm_phrase = parser(phrase)
+        # print(norm_phrase)
+        handle(norm_phrase)
+
 
 if __name__ == "__main__":
-	main()
+    main()
